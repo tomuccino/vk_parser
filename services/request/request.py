0372bdb70e8ea5208a1a2e82b0707b90330053fe
tomuccino <fillippok87@gmail.com>
2026-09-12 22:25:33 +0700

    Request.check_url_head return UrlStatus

--- services/request/request.py
+++ services/request/request.py
@@ -2,7 +2,7 @@ import requests
 import logging
 
 from dataclasses import dataclass
-from utils.logger import init_logger
+from services.request.url_status import UrlStatus
 
 logger = logging.getLogger(__name__)
 
@@ -14,17 +14,27 @@ class Request:
     timeout: int = 5
     allow_redirects: bool = True
 
-    def check_url_head(self, url):
+    def check_url_head(self, url) -> UrlStatus:
         logger.debug(url)
         try:
             response = requests.head(
                 url, timeout=self.timeout, allow_redirects=self.allow_redirects
             )
-            return response.status_code == 200
+            # return response.status_code == 200
+            logger.info(
+                "check url: %s -> %s (final url: %s)",
+                url,
+                response.status_code,
+                response.url,
+            )
+            return UrlStatus(
+                url=url,
+                available=response.status_code == 200,
+                status_code=response.status_code,
+            )
         except requests.exceptions.RequestException as e:
             logger.exception(e)
-            return False
-
+            return UrlStatus(url=url, available=False, status_code=None, error=str(e))
 
 
 def main():
